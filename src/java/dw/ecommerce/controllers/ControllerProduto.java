fb9e54aeb7d90c69ca32529f3bc51ac264062156
brunodutr <brun_dutra@hotmail.com>
2017-10-29 13:50:47 -0200

    DW - UFF

---
+++ src/java/dw/ecommerce/controllers/ControllerProduto.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dw.ecommerce.controllers;
+
+import dw.ecommerce.logica.produto.Logica;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Bruno Dutra
+ */
+@WebServlet("/produto")
+public class ControllerProduto extends HttpServlet{
+    
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+        String parametro = request.getParameter("logica");
+        String nomeDaClasse = "dw.ecommerce.logica.produto." + parametro;
+        
+        try {
+            Class classe = Class.forName(nomeDaClasse);
+            Logica logica = (Logica) classe.newInstance();
+            String pagina = logica.executa(request, response);
+            
+            request.getRequestDispatcher(pagina).forward(request, response);
+            
+           
+        } catch (Exception e) {
+            throw new ServletException("O sistema do ecommerce apresentou algum problema.");
+        }
+                
+    }
+}
