ef5560ff8156cf42673b3517e11fb9c198693164
brunodutr <brun_dutra@hotmail.com>
2017-11-04 01:02:00 -0200

    Servlet para adicionar novo produto.

---
+++ src/main/java/dw/ecommerce/controller/NovoProdutoServlet.java
@@ -0,0 +1,39 @@
+package dw.ecommerce.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dw.ecommerce.dao.CategoriaDAO;
+import dw.ecommerce.modelo.Categoria;
+
+/**
+ * Servlet implementation class NovoProdutoServlet
+ */
+@WebServlet("/NovoProduto")
+public class NovoProdutoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Connection connection = (Connection) request.getAttribute("conexao");
+		
+		List<Categoria> categorias = new CategoriaDAO(connection).getLista();
+		request.setAttribute("categorias", categorias);
+		
+		request.getRequestDispatcher("WEB-INF/views/painel-admin/produto/form.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
